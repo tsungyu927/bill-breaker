b33c9e3b4e307a2a0336af3644f5815dbfce539f
tsungyu927 <tsungyu927@gmail.com>
2024-10-26 11:53:43 +0800

    feat: init router for web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "@/src/routes/Home";
+import ErrorPage from "@/src/routes/Error";
 
-createRoot(document.getElementById('root')!).render(
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routers} />
+  </StrictMode>
+);
