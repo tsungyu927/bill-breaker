5cb6cd3eeb640ce922446d91486a7ece560ea149
tsungyu927 <tsungyu927@gmail.com>
2024-11-03 18:01:14 +0800

    feat(web): init shadcn with tailwindcss

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "@/src/App";
+import "./main.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
