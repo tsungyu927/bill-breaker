abe096fd5709441c841eb06ba300f990d21d8d9f
tsungyu927 <tsungyu927@gmail.com>
2024-11-09 14:51:22 +0800

    feat(api): insert book_members table after create the new book

--- apps/api/models/book.go
+++ apps/api/models/book.go
@@ -16,10 +16,16 @@ type BookModel struct {
 	LastModifiedAt  time.Time `json:"last_modified_at"`
 }
 
+type BookMemberModel struct {
+	BookID   string    `json:"book_id"`
+	UserID   string    `json:"user_id"`
+	JoinedAt time.Time `json:"joined_at"`
+}
+
 func (book *BookModel) Create() error {
 	query := `INSERT INTO books (creator_id, book_name, book_description) VALUES ($1, $2, $3) RETURNING *`
 
-	return db.GetDB().QueryRow(
+	err := db.GetDB().QueryRow(
 		context.Background(),
 		query,
 		book.CreatorID,
@@ -33,4 +39,22 @@ func (book *BookModel) Create() error {
 		&book.CreatedAt,
 		&book.LastModifiedAt,
 	)
+	if err != nil {
+		return err
+	}
+
+	// Add the creator as a member of the book (to book_members table)
+	err = addBookMember(book.ID, book.CreatorID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addBookMember(bookID, userID string) error {
+	query := `INSERT INTO book_members (book_id, user_id) VALUES ($1, $2)`
+
+	_, err := db.GetDB().Exec(context.Background(), query, bookID, userID)
+	return err
 }
